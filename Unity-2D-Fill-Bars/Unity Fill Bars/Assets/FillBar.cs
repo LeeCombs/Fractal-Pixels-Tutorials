aa2aa9b9b0d7fbc22e6a1d079dedc36b12d23b2a
HellaBored <leecombs_30@hotmail.com>
2018-09-25 16:43:15 -0400

    Unity Fill Bars Tutorial Project Files

---
+++ Unity-2D-Fill-Bars/Unity Fill Bars/Assets/FillBar.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FillBar : MonoBehaviour {
+    
+    // Unity UI References
+    public Slider slider;
+    public Text displayText;
+    
+    // Create a property to handle the slider's value
+    private float currentValue = 0f;
+    public float CurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            currentValue = value;
+            slider.value = currentValue;
+            displayText.text = (slider.value * 100).ToString("0.00") + "%";
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        CurrentValue = 0f;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        CurrentValue += 0.0043f;
+    }
+}
