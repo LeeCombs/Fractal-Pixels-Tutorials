da9b0d2fcc6b5714e8e7cd58e412d6fbe29c7447
HellaBored <leecombs_30@hotmail.com>
2018-09-28 18:33:31 -0400

    Progress Bar and Radial Bar

---
+++ Unity-2D-Fill-Bars/Unity Fill Bars/Assets/ProgressBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class ProgressBar : FillBar {
+
+    // Event to invoke when the progress bar fills up
+    private UnityEvent onProgressComplete;
+
+    // Create a property to handle the slider's value
+    public new float CurrentValue {
+        get {
+            return base.CurrentValue;
+        }
+        set {
+            // If the value exceeds the max fill, invoke the completion function
+            if (value > slider.maxValue)
+                onProgressComplete.Invoke();
+
+            // Remove any overfill (i.e. 105% fill -> 5% fill)
+            base.CurrentValue = value % slider.maxValue;
+        }
+    }
+    
+    void Start () {
+        // Initialize onProgressComplete and set a basic callback
+        if (onProgressComplete == null)
+            onProgressComplete = new UnityEvent();
+        onProgressComplete.AddListener(OnProgressComplete);
+    }
+	
+    void Update () {
+        CurrentValue += 0.0153f;
+    }
+
+    // The method to call when the progress bar fills up
+    void OnProgressComplete() {
+        Debug.Log("Progress Complete");
+    }
+}
---
+++ Unity-2D-Fill-Bars/Unity Fill Bars/Assets/RadialProgressBar.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialProgressBar : MonoBehaviour {
+
+    // Public UI References
+    public Image fillImage;
+    public Text displayText;
+
+    // Create a property to handle the slider's value
+    private float currentValue = 0f;
+    public float CurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            currentValue = Mathf.Min(value, 1);
+            fillImage.fillAmount = currentValue;
+            displayText.text = (currentValue * 100).ToString("0.00") + "%";
+        }
+    }
+
+    void Start() {
+        CurrentValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CurrentValue += 0.0043f;
+    }
+}
