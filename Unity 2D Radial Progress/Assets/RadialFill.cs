86dd0955c27bb10c91c50a6dc489925416ff5b2c
HellaBored <leecombs_30@hotmail.com>
2018-10-03 12:47:45 -0400

    Radial Progress

---
+++ Unity 2D Radial Progress/Assets/RadialFill.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialFill : MonoBehaviour {
+
+    // Public UI References
+    public Image fillImage;
+    public Text displayText;
+
+    // Trackers for min/max values
+    protected float maxValue = 2f, minValue = 0f;
+
+    // Create a property to handle the slider's value
+    private float currentValue = 0f;
+    public float CurrentValue {
+        get {
+            return currentValue;
+        }
+        set {
+            // Ensure the passed value falls within min/max range
+            currentValue = Mathf.Clamp(value, minValue, maxValue);
+
+            // Calculate the current fill percentage and display it
+            float fillPercentage = currentValue / maxValue;
+            fillImage.fillAmount = fillPercentage;
+            displayText.text = (fillPercentage * 100).ToString("0.00") + "%";
+        }
+    }
+
+    void Start() {
+        CurrentValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CurrentValue += 0.0086f;
+    }
+}
---
+++ Unity 2D Radial Progress/Assets/RadialProgress.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class RadialProgress : RadialFill {
+
+    // Event to invoke when the progress bar fills up
+    private UnityEvent onProgressComplete;
+
+    // Create a property to handle the slider's value
+    public new float CurrentValue {
+        get {
+            return base.CurrentValue;
+        }
+        set {
+            // If the value exceeds the max fill, invoke the completion function
+            if (value > maxValue)
+                onProgressComplete.Invoke();
+
+            // Remove any overfill (i.e. 105% fill -> 5% fill)
+            base.CurrentValue = value % maxValue;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        // Initialize onProgressComplete and set a basic callback
+        if (onProgressComplete == null)
+            onProgressComplete = new UnityEvent();
+        onProgressComplete.AddListener(OnProgressComplete);
+    }
+	
+    // Update is called once per frame
+    void Update () {
+        CurrentValue += 0.0273f;
+    }
+
+    // The method to call when the progress bar fills up
+    void OnProgressComplete() {
+        Debug.Log("Progress Complete");
+    }
+}
